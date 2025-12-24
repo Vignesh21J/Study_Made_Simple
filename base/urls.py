ab8d91d55f0cbd6935c3dde42c4cff960b7bab68
Vignesh21J <vignesh11212003@gmail.com>
2025-12-24 14:24:57 +0530

    feat: added login and logout functionalities

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.RegisterUser, name='register-user'),
+    path('login/', views.LoginUser, name='login-user'),
+
+    path('logout/', views.LogoutUser, name='logout-user'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import RegisterUserForm
 from django.contrib import messages
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -27,3 +32,36 @@ def RegisterUser(request):
     }
 
     return render(request, 'base/register.html', context)
+
+def LoginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        email = request.POST.get('email').strip().lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+
+        except User.DoesNotExist:
+            messages.error(request, "User with this Email does not exist..!")
+            return render(request, 'base/login.html')
+        
+        user = authenticate(request=request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged In..!")
+            return redirect('home')
+        else:
+            messages.error(request, "Email or Password does not exist..!")
+        
+
+    return render(request, 'base/login.html')
+
+
+def LogoutUser(request):
+    logout(request)
+    return redirect('home')
